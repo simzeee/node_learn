c7b70bc21bde7901ed714ea924a564cd30de10a6
John Sims <johnsims.dev@gmail.com>
2025-03-23 23:58:36 -0400

    chore: finish message to .txt

--- first_server/app.js
+++ first_server/app.js
@@ -1,7 +1,11 @@
 import http from "http";
+import fs from "fs";
+import { parse } from "path";
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Ender Message</title></head>");
@@ -9,6 +13,24 @@ const server = http.createServer((req, res) => {
       `<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>`
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method == "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-type", "text/html");
